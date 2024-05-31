a0ce7751acc01052d97713a9069c878705286831
GAILLARD Maxime <maxime.gaillard1@outlook.fr>
2024-05-31 11:55:21 +0200

    Adding StateMachineComponent

---
+++ Source/State_Machine/Private/StateMachineComponent.cpp
@@ -0,0 +1,126 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StateMachineComponent.h"
+#include "Net/UnrealNetwork.h"
+#include "Engine/Engine.h"
+
+// Sets default values for this component's properties
+UStateMachineComponent::UStateMachineComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	//Enable by default the replication on the state machine
+	SetIsReplicatedByDefault(true);
+}
+
+
+// Called when the game starts
+void UStateMachineComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void UStateMachineComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// If the tick is enabled then execute the tick for the current state.
+	// If it's not enabled, it's mean that the state machine is currently on a state transition
+	if(bCanUseTick)
+	{
+		TickState(DeltaTime);
+	}
+
+	// If bUseDebug == true then execute the Debug function
+	// But only when inside the editor.
+#if WITH_EDITOR
+	if(bUseDebug)
+	{
+		Debug();
+	}
+#endif
+}
+
+void UStateMachineComponent::SwitchState(FGameplayTag NewState)
+{
+	// Call the switch state on the server for replication purpose
+	Server_SwitchState(NewState);
+}
+
+void UStateMachineComponent::Server_SwitchState_Implementation(FGameplayTag NewState)
+{
+	// Call the switch state on all of the connected clients.
+	Client_SwitchState(NewState);
+}
+
+void UStateMachineComponent::Client_SwitchState_Implementation(FGameplayTag NewState)
+{
+	// Leave the function if the new state isn't an accepted one
+	if(!AcceptedStates.Contains(NewState) && !InitialState.MatchesTagExact(NewState))
+	{
+		if(bUseDebug)
+			UE_LOG(LogTemp, Warning, TEXT("Couldn't switch state for %s because the state %s isn't in accepted state array"), *GetOwner()->GetName(), *NewState.ToString());
+		return;
+	}
+
+	// Leave the function if the new state the same as the current one
+	if(CurrentState.MatchesTagExact(NewState))
+	{
+		if(bUseDebug)
+			UE_LOG(LogTemp, Warning, TEXT("Couldn't switch state for %s because it is already in %s state"), *GetOwner()->GetName(), *NewState.ToString());
+		return;
+	}
+
+	//Stop the tick when changing the state in case of the transition takes too much time
+	bCanUseTick = false;
+	//First, exit the current state
+	ExitState();
+	//Then, set the new state
+	CurrentState = NewState;
+	//Finally, enter the new state
+	EnterState();
+	//Reenable the tick 
+	bCanUseTick = true;
+}
+
+void UStateMachineComponent::EnterState()
+{
+	// If the enter state delegate is bound to something, then broadcast the delegate
+	if(StateMachine_EnterState.IsBound())
+	{
+		StateMachine_EnterState.Broadcast(CurrentState);
+	}
+}
+
+void UStateMachineComponent::TickState(float DeltaTime)
+{
+	// If the tick state delegate is bound to something, then broadcast the delegate
+	if(StateMachine_TickState.IsBound())
+	{
+		StateMachine_TickState.Broadcast(DeltaTime, CurrentState);
+	}
+}
+
+void UStateMachineComponent::ExitState()
+{
+	// If the exit state delegate is bound to something, then broadcast the delegate
+	if(StateMachine_ExitState.IsBound())
+	{
+		StateMachine_ExitState.Broadcast(CurrentState);
+	}
+}
+
+void UStateMachineComponent::Debug()
+{
+	if(GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(DebugKey, 0.0f, FColor::Cyan, FString::Printf(TEXT("Current State for %s: %s"), *GetOwner()->GetName(), *CurrentState.ToString()));
+	}
+}
+
+
+
---
+++ Source/State_Machine/Public/StateMachineComponent.h
@@ -0,0 +1,116 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "GameplayTags.h"
+#include "StateMachineComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FEnterStateSignature, FGameplayTag, StateTag);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FExitStateSignature, FGameplayTag, StateTag);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FTickStateSignature, float, DeltaTime, FGameplayTag, StateTag);
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class STATE_MACHINE_API UStateMachineComponent : public UActorComponent
+{
+	GENERATED_BODY()
+	
+protected:
+	/// The initial state of the state machine. 
+	UPROPERTY(EditAnywhere, Category = "State Machine")
+	FGameplayTag InitialState;
+	
+	/// The current state of the state machine.
+	UPROPERTY()
+	FGameplayTag CurrentState;
+	
+	/// The array of accepted states. If the state machine tries to change to a state
+	/// that isn't contain in the accepted state, it won't work 
+	UPROPERTY(EditAnywhere, Category = "State Machine")
+	TArray<FGameplayTag> AcceptedStates;
+
+	/// If True, enables the debug of the plugin.
+	UPROPERTY(EditAnywhere, Category = "State Machine")
+	bool bUseDebug;
+	
+	/// The key for the debug log of the current state.
+	UPROPERTY(EditAnywhere, Category = "State Machine", meta = (EditCondition="bUseDebug", EditConditionHides))
+	int8 DebugKey;
+
+	
+
+public:	
+	/// Sets default values for this component's properties
+	UStateMachineComponent();
+	
+	/// Delegate for the enter state method
+	UPROPERTY(BlueprintAssignable)
+	FEnterStateSignature StateMachine_EnterState;
+	
+	/// Delegate for the exit state method
+	UPROPERTY(BlueprintAssignable)
+	FExitStateSignature StateMachine_ExitState;
+	
+	/// Delegate for the tick state method
+	UPROPERTY(BlueprintAssignable)
+	FTickStateSignature StateMachine_TickState;
+
+
+protected:
+	/// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	/// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	/// Function used for changing the state of the state machine. It's the only one that should be manually called
+	/// @param NewState The new state for the state machine
+	UFUNCTION(BlueprintCallable, Category = "State Machine")
+	void SwitchState(FGameplayTag NewState);
+
+	/// Return the initial state of the state machine
+	/// @return The initial state property
+	UFUNCTION(BlueprintPure, BlueprintCallable, Category = "State Machine")
+	FORCEINLINE FGameplayTag GetInitialState() const { return InitialState; }
+
+	/// Return the current state of the state machine
+	/// @return The current state property
+	UFUNCTION(BlueprintCallable, Category = "State Machine")
+	FORCEINLINE FGameplayTag GetCurrentState() const { return CurrentState; }
+
+	/// Return the array of accepted state
+	/// @return The accepted state property
+	UFUNCTION(BlueprintCallable, Category = "State Machine")
+	FORCEINLINE TArray<FGameplayTag> GetAcceptedStates() const { return AcceptedStates; }
+	
+private:
+	/// Function used for passing the changing state to the server for replication. Called by SwitchState function.
+	/// @param NewState The new state for the state machine 
+	UFUNCTION(Server, Reliable)
+	void Server_SwitchState(FGameplayTag NewState);
+
+	/// Function used for passing from the server to any connected clients the changing state. Called by Server_SwitchState function.
+	/// @param NewState The new state for the state machine
+	UFUNCTION(NetMulticast, Reliable)
+	void Client_SwitchState(FGameplayTag NewState);
+
+	/// Boolean that control the tick state event. Should be false while changing state to avoid undesired effects.
+	bool bCanUseTick = false;
+
+	/// Function triggered when a new state is entered
+	void EnterState();
+
+	/// Function triggered on tick
+	/// @param DeltaTime The delta of the Component Tick.
+	void TickState(float DeltaTime);
+
+	/// Function triggered when the current state is exited
+	void ExitState();
+
+public:	
+
+	/// Function used for debugging the state machine
+	virtual void Debug();
+};
